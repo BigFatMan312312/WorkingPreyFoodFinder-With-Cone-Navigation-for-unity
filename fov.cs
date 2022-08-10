14b17ac90166c14de8a38aa4f7cfb766a1a0a7ea
BigFatMan312312 <108171724+BigFatMan312312@users.noreply.github.com>
2022-08-09 19:38:12 -0700

    this is the main file. there may be more coming.
    the code is really easy, just give the script an navmesh agent and since the script has a boolean for that you just hook that boolean up to where it will go. this can be applyed to really anything if you want it to. if you want a tutorial please download this, the more downloads the more updates and tutorials i will do.

---
+++ fov.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class fov : MonoBehaviour
+{
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+
+    public NavMeshAgent agent;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+
+
+
+
+
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    canSeePlayer = true;
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+
+            if(canSeePlayer) 
+            {
+                if(target.gameObject.tag == "Food") 
+                {
+                    agent.SetDestination(target.position);
+                }
+            } else if(!canSeePlayer) 
+            {
+                float Timer = 0;
+
+                if(Timer >= 10) 
+                {
+                    agent.SetDestination(Random.insideUnitCircle);
+                }
+
+                if(Timer < 10) 
+                {
+                    Timer += 0.05f;
+                }
+            }
+        }
+        else if (canSeePlayer)
+            canSeePlayer = false;
+
+        if(rangeChecks.Length == 0)
+        {
+                float Timer = 0;
+
+                if(Timer >= 10) 
+                {
+                    agent.SetDestination(Random.insideUnitCircle);
+                }
+
+                if(Timer < 10) 
+                {
+                    Timer += 0.05f;
+                }
+        }
+
+   
+    }
+
+    public void Update() 
+    {
+        
+    }
+}
